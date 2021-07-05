37ce6357e6a5cd5a2b998c90e5f3af139c638943
snowing222 <snowing222@gmail.com>
2021-07-05 09:55:52 -0400

    add comment

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,5 +1,7 @@
 const { gql }  = require('apollo-server')
 
+//Post type: defines GraphQL schema Post.
+//query type: it lists all of the available queries that. clients can execute, along with the return type for each.
 module.exports = gql` 
     type Post {
         id:ID!
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+//apollo-server is the core library, which helps you define the shape of your data and how to fetch it.
 const { ApolloServer } = require('apollo-server');
 //connect mongo database
 const mongoose = require('mongoose');
 
-
+// typeDef to define dataset
+// Resolvers tell Apollo Server how to fetch the data associated with a particular type.
 const typeDefs = require('./graphql/typeDefs')
 const resolvers = require('./graphql/resolvers')
 const { MONGODB } = require('./config.js')
