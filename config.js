954c70e477e8916039ba2fd76c812cf8c4fcae04
snowing222 <snowing222@gmail.com>
2021-07-05 12:40:43 -0400

    add regist user

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    MONGODB: 'mongodb+srv://Elsa:Iloveyou8422@cluster0.ei6vc.mongodb.net/mango?retryWrites=true&w=majority'
-}
+  MONGODB:
+    "mongodb+srv://Elsa:Iloveyou8422@cluster0.ei6vc.mongodb.net/mango?retryWrites=true&w=majority",
+  SECRET_KEY: "some very secret key",
+};
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -10,7 +10,23 @@ module.exports = gql`
         username: String!
 
     }
+    type User{
+        id:ID!
+        email: String!
+        token: String!
+        username: String!
+        createAt: String!
+    }
+    input RegisterInput{
+        username: String!
+        password: String!
+        confirmPassword: String!
+        email: String!
+    }
     type Query{
         getPosts: [Post]
+    }
+    type Mutation{
+        register(registerInput:RegisterInput): User!
     }
 `
