97956a854a9698c01ac3371590d6352df56fedfc
snowing222 <snowing222@gmail.com>
2021-07-08 20:19:12 -0400

    add checkauth js

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -24,7 +24,7 @@ module.exports = {
                 throw new UserInputError('Errors', {errors}) 
             }
 
-            const user = await User.findOne({ username});
+            const user = await User.findOne({username});
             if(!user){
                 errors.general = 'User not found';
                 throw new UserInputError('User not found', {errors})
